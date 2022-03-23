df2a8d7b9af39945f34babf4e25413082d42fd41
nazareth133 <nando_nazareth@hotmail.com>
2022-03-23 14:46:36 -0300

    Adicao do lombok

--- src/main/java/com/sicred/receita/dto/ContaDTO.java
+++ src/main/java/com/sicred/receita/dto/ContaDTO.java
@@ -1,6 +1,8 @@
 package com.sicred.receita.dto;
 
+import lombok.Data;
 
+@Data
 public class ContaDTO {
 
    private String agencia;
@@ -9,48 +11,5 @@ public class ContaDTO {
    private String status;
    private String validoBacen;
 
-   public String getStatus() {
-      return status;
-   }
-
-   public void setStatus(String status) {
-      this.status = status;
-   }
-
-   public String getAgencia() {
-      return agencia;
-   }
-
-   public void setAgencia(String agencia) {
-      this.agencia = agencia;
-   }
-
-   public String getConta() {
-      return conta;
-   }
-
-   public void setConta(String conta) {
-      this.conta = conta;
-   }
-
-
-
-   public String getSaldo() {
-      return saldo;
-   }
-
-   public void setSaldo(String saldo) {
-      this.saldo = saldo;
-   }
-
-
-
-   public String getValidoBacen() {
-      return validoBacen;
-   }
-
-   public void setValidoBacen(String validoBacen) {
-      this.validoBacen = validoBacen;
-   }
 
 }
